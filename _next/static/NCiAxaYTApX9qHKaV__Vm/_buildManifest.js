3917f8d73e3463a082872adf4bc9219a3acf1633
CyberSecurity Group <groupcybersecurity@gmail.com>
2026-08-11 00:58:45 -0400

    Deploy: build sin basePath + CNAME cybersecuritygroup.cl

--- _next/static/NCiAxaYTApX9qHKaV__Vm/_buildManifest.js
+++ _next/static/NCiAxaYTApX9qHKaV__Vm/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/cybersecuritygroup-nextjs//_next/:path+",
-        "destination": "/cybersecuritygroup-nextjs/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
